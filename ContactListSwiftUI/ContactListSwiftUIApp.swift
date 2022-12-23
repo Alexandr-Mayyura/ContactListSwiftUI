bb2ed4fd33fc24eefef79b7185a94301ec170bd2
Alexandr-Mayyura <sonickill@yandex.ru>
2022-12-23 19:11:57 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Aleksandr Mayyura on 23.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
